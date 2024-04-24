5070cea5d4f34fff0d7f4d2a2df714766237f8ea
Saheed Lawanson <saheedlawanson47@gmail.com>
2024-04-24 18:13:37 +0100

    Update auth.js

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -187,6 +187,7 @@ export const changePassword = async (event) => {
 
 
 export const resetPassword = async (event) => {
+  console.log("EVENT:", event)
   const eventBody = JSON.parse(event.body);
 
   try {
